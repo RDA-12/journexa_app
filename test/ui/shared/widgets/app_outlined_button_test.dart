786ebea3ea128491ea90a5e6906da84b4630d884
R-12 <ridwandani12@gmail.com>
2026-08-13 19:09:35 +0700

    test: ButtonSize varied test

--- test/ui/shared/widgets/app_outlined_button_test.dart
+++ test/ui/shared/widgets/app_outlined_button_test.dart
@@ -8,6 +8,7 @@ void main() {
     required String label,
     Widget? icon,
     VoidCallback? onPressed,
+    ButtonSize size = ButtonSize.small,
   }) async {
     await tester.pumpWidget(
       MaterialApp(
@@ -16,6 +17,7 @@ void main() {
             label: label,
             icon: icon,
             onPressed: onPressed,
+            size: size,
           ),
         ),
       ),
@@ -23,112 +25,127 @@ void main() {
   }
 
   group('Render', () {
-    testWidgets('shows label', (tester) async {
-      await pumpWidget(tester: tester, label: 'Hello');
+    for (final size in ButtonSize.values) {
+      testWidgets('shows label for size $size', (tester) async {
+        await pumpWidget(tester: tester, label: 'Hello', size: size);
 
-      expect(find.text('Hello'), findsOneWidget);
-    });
+        expect(find.text('Hello'), findsOneWidget);
+      });
 
-    testWidgets(
-      'shows icon when provided',
-      (tester) async {
-        await pumpWidget(
-          tester: tester,
-          label: 'Hello',
-          icon: const Icon(Icons.add),
-        );
+      testWidgets(
+        'shows icon when provided for size $size',
+        (tester) async {
+          await pumpWidget(
+            tester: tester,
+            label: 'Hello',
+            icon: const Icon(Icons.add),
+            size: size,
+          );
 
-        expect(find.byIcon(Icons.add), findsOneWidget);
-      },
-    );
-
-    testWidgets('disabled when onPressed is null', (tester) async {
-      await pumpWidget(tester: tester, label: 'Hello');
-
-      final button = tester.widget<OutlinedButton>(
-        find.byType(OutlinedButton),
+          expect(find.byIcon(Icons.add), findsOneWidget);
+        },
       );
 
-      expect(button.onPressed, null);
-    });
+      testWidgets(
+        'disabled when onPressed is null for size $size',
+        (tester) async {
+          await pumpWidget(tester: tester, label: 'Hello', size: size);
+
+          final button = tester.widget<OutlinedButton>(
+            find.byType(OutlinedButton),
+          );
+
+          expect(button.onPressed, null);
+        },
+      );
+    }
   });
 
   group('Interaction', () {
-    testWidgets('Calls onPressed when pressed', (tester) async {
-      var isPressed = false;
-      await pumpWidget(
-        tester: tester,
-        label: 'Hello',
-        onPressed: () => isPressed = true,
+    for (final size in ButtonSize.values) {
+      testWidgets(
+        'Calls onPressed when pressed for size $size',
+        (tester) async {
+          var isPressed = false;
+          await pumpWidget(
+            tester: tester,
+            label: 'Hello',
+            onPressed: () => isPressed = true,
+          );
+
+          await tester.tap(find.byType(AppOutlinedButton));
+
+          expect(isPressed, true);
+        },
       );
-
-      await tester.tap(find.byType(AppOutlinedButton));
-
-      expect(isPressed, true);
-    });
+    }
   });
 
   group('a11y', () {
-    testWidgets(
-      'follows a11y guidelines when label only',
-      (tester) async {
-        final handle = tester.ensureSemantics();
-        await pumpWidget(tester: tester, label: 'Hello');
+    for (final size in ButtonSize.values) {
+      testWidgets(
+        'follows a11y guidelines when label only for size $size',
+        (tester) async {
+          final handle = tester.ensureSemantics();
+          await pumpWidget(tester: tester, label: 'Hello', size: size);
 
-        await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
-        await expectLater(tester, meetsGuideline(iOSTapTargetGuideline));
-        await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
-        await expectLater(tester, meetsGuideline(textContrastGuideline));
+          await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
+          await expectLater(tester, meetsGuideline(iOSTapTargetGuideline));
+          await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
+          await expectLater(tester, meetsGuideline(textContrastGuideline));
 
-        handle.dispose();
-      },
-    );
+          handle.dispose();
+        },
+      );
 
-    testWidgets(
-      'follows a11y guidelines when icon is provided',
-      (tester) async {
-        final handle = tester.ensureSemantics();
-        await pumpWidget(
-          tester: tester,
-          label: 'Hello',
-          icon: const Icon(Icons.add),
-        );
+      testWidgets(
+        'follows a11y guidelines when icon is provided for size $size',
+        (tester) async {
+          final handle = tester.ensureSemantics();
+          await pumpWidget(
+            tester: tester,
+            label: 'Hello',
+            size: size,
+            icon: const Icon(Icons.add),
+          );
 
-        await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
-        await expectLater(tester, meetsGuideline(iOSTapTargetGuideline));
-        await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
-        await expectLater(tester, meetsGuideline(textContrastGuideline));
+          await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
+          await expectLater(tester, meetsGuideline(iOSTapTargetGuideline));
+          await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
+          await expectLater(tester, meetsGuideline(textContrastGuideline));
 
-        handle.dispose();
-      },
-    );
+          handle.dispose();
+        },
+      );
 
-    testWidgets(
-      'has correct label when semanticsLabel is provided',
-      (tester) async {
-        final handle = tester.ensureSemantics();
-        await pumpWidget(tester: tester, label: 'Hello');
+      testWidgets(
+        'has correct label when semanticsLabel is provided for size $size',
+        (tester) async {
+          final handle = tester.ensureSemantics();
+          await pumpWidget(tester: tester, label: 'Hello', size: size);
 
-        expect(find.bySemanticsLabel('Hello'), findsOneWidget);
+          expect(find.bySemanticsLabel('Hello'), findsOneWidget);
 
-        handle.dispose();
-      },
-    );
+          handle.dispose();
+        },
+      );
 
-    testWidgets(
-      'has correct label when icon is provided',
-      (tester) async {
-        final handle = tester.ensureSemantics();
-        await pumpWidget(
-          tester: tester,
-          label: 'Hello',
-          icon: const Icon(Icons.add),
-        );
+      testWidgets(
+        'has correct label when icon is provided for size $size',
+        (tester) async {
+          final handle = tester.ensureSemantics();
+          await pumpWidget(
+            tester: tester,
+            label: 'Hello',
+            size: size,
+            icon: const Icon(Icons.add),
+          );
 
-        expect(find.bySemanticsLabel('Hello'), findsOneWidget);
+          expect(find.bySemanticsLabel('Hello'), findsOneWidget);
 
-        handle.dispose();
-      },
-    );
+          handle.dispose();
+        },
+      );
+    }
   });
 }

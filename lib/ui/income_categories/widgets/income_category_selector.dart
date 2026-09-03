f49587e626faf722c55e18b17a70b0f98f549172
R-12 <ridwandani12@gmail.com>
2026-09-03 17:30:18 +0700

    feat: selector to select IncomeCategory

---
+++ lib/ui/income_categories/widgets/income_category_selector.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:journexa_app/domain/entities/income_category.dart';
+import 'package:journexa_app/ui/income_categories/bloc/income_categories_bloc.dart';
+import 'package:journexa_app/ui/shared/widgets/app_selector.dart';
+
+/// Creates new [AppSelector] to select an [IncomeCategory]
+class IncomeCategorySelector extends StatefulWidget {
+  /// Creates new [IncomeCategorySelector]
+  ///
+  /// It uses [IncomeCategoriesBloc] to search and fetch income categories.
+  /// So, make sure to provide it with that bloc.
+  const IncomeCategorySelector({
+    required this.isRequired,
+    this.label,
+    this.initialItems = const [],
+    this.controller,
+    this.initialValue,
+    super.key,
+  });
+
+  /// Whether this income category selector is required
+  final bool isRequired;
+
+  /// Optional label for this income category selector
+  final String? label;
+
+  /// Optional controller for this income category selector
+  ///
+  /// Creates new one when null
+  final AppSelectorController<IncomeCategory>? controller;
+
+  /// Initial items to display in the income category selector
+  ///
+  /// Defaults to an empty list
+  final List<IncomeCategory> initialItems;
+
+  /// Initial value to be selected
+  final IncomeCategory? initialValue;
+
+  @override
+  State<IncomeCategorySelector> createState() => _IncomeCategorySelectorState();
+}
+
+class _IncomeCategorySelectorState extends State<IncomeCategorySelector> {
+  late final AppSelectorController<IncomeCategory> _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        widget.controller ??
+        AppSelectorController<IncomeCategory>(
+          displayAsString: (it) => it.name,
+        );
+    if (widget.initialItems.isNotEmpty) {
+      _controller.items = widget.initialItems;
+    }
+    if (widget.initialValue != null) {
+      _controller.value = widget.initialValue;
+    }
+  }
+
+  @override
+  void dispose() {
+    if (widget.controller == null) {
+      _controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<IncomeCategoriesBloc, IncomeCategoriesState>(
+      listenWhen: (p, c) => p.status != c.status,
+      listener: (context, state) {
+        switch (state.status) {
+          case IncomeCategoriesStatus.loading:
+            _controller.isLoading = true;
+          case IncomeCategoriesStatus.failure:
+            _controller.lastError = state.exception;
+          case IncomeCategoriesStatus.loaded:
+            _controller.isLoading = false;
+            _controller.items =
+                state.categories.map((it) => it.category).toList();
+          case _:
+        }
+      },
+      child: AppSelector(
+        controller: _controller,
+        isRequired: widget.isRequired,
+        label: widget.label,
+        onPressed: () {
+          context.read<IncomeCategoriesBloc>().add(
+            const IncomeCategoriesEvent.load(),
+          );
+        },
+        onSearch: (query) {
+          context.read<IncomeCategoriesBloc>().add(
+            IncomeCategoriesEvent.search(query: query ?? ''),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ test/ui/income_categories/widgets/income_category_selector_test.dart
@@ -0,0 +1,261 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:journexa_app/domain/entities/income_category.dart';
+import 'package:journexa_app/shared/app_exception.dart';
+import 'package:journexa_app/ui/income_categories/bloc/income_categories_bloc.dart';
+import 'package:journexa_app/ui/income_categories/widgets/income_category_selector.dart';
+import 'package:journexa_app/ui/shared/widgets/app_selector.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../util.dart';
+
+class MockIncomeCategoriesBloc extends Mock implements IncomeCategoriesBloc {}
+
+void main() {
+  late IncomeCategoriesBloc mockIncomeCategoriesBloc;
+
+  setUp(() {
+    mockIncomeCategoriesBloc = MockIncomeCategoriesBloc();
+    whenListen(
+      mockIncomeCategoriesBloc,
+      const Stream<IncomeCategoriesState>.empty(),
+      initialState: const IncomeCategoriesState(),
+    );
+  });
+
+  Future<void> pumpWidget(
+    WidgetTester tester, {
+    AppSelectorController<IncomeCategory>? controller,
+    String? label,
+    bool? isRequired,
+    List<IncomeCategory>? initialItems,
+    IncomeCategory? initialValue,
+  }) {
+    return pumpForWidgetTest(
+      tester,
+      locale: const Locale('en'),
+      widget: BlocProvider.value(
+        value: mockIncomeCategoriesBloc,
+        child: IncomeCategorySelector(
+          isRequired: isRequired ?? false,
+          label: label,
+          controller: controller,
+          initialItems: initialItems ?? [],
+          initialValue: initialValue,
+        ),
+      ),
+    );
+  }
+
+  group('Render', () {
+    testWidgets(
+      'shows correct AppSelector when isRequired = true',
+      (tester) async {
+        await pumpWidget(tester, isRequired: true);
+
+        final widgetFinder = find.byType(AppSelector<IncomeCategory>);
+        expect(widgetFinder, findsOneWidget);
+
+        final widget =
+            tester.widget<AppSelector<IncomeCategory>>(widgetFinder);
+        expect(widget.isRequired, true);
+      },
+    );
+
+    testWidgets(
+      'shows correct AppSelector when isRequired = false',
+      (tester) async {
+        await pumpWidget(tester, isRequired: false);
+
+        final widgetFinder = find.byType(AppSelector<IncomeCategory>);
+        expect(widgetFinder, findsOneWidget);
+
+        final widget =
+            tester.widget<AppSelector<IncomeCategory>>(widgetFinder);
+        expect(widget.isRequired, false);
+      },
+    );
+
+    testWidgets(
+      'shows correct AppSelector when label = some text',
+      (tester) async {
+        await pumpWidget(tester, label: 'Some text');
+
+        final widgetFinder = find.byType(AppSelector<IncomeCategory>);
+        expect(widgetFinder, findsOneWidget);
+
+        final widget =
+            tester.widget<AppSelector<IncomeCategory>>(widgetFinder);
+        expect(widget.label, 'Some text');
+      },
+    );
+
+    testWidgets(
+      'shows correct AppSelector when controller is provided',
+      (tester) async {
+        final controller = AppSelectorController<IncomeCategory>(
+          displayAsString: (it) => it.name,
+        );
+        await pumpWidget(tester, controller: controller);
+
+        final widgetFinder = find.byType(AppSelector<IncomeCategory>);
+        expect(widgetFinder, findsOneWidget);
+
+        final widget =
+            tester.widget<AppSelector<IncomeCategory>>(widgetFinder);
+        expect(widget.controller, controller);
+      },
+    );
+
+    testWidgets(
+      'set initialItems to controller',
+      (tester) async {
+        final items = List<IncomeCategory>.generate(
+          2,
+          (i) => IncomeCategory.test(),
+        );
+        final controller = AppSelectorController<IncomeCategory>(
+          displayAsString: (it) => it.name,
+        );
+        await pumpWidget(tester, initialItems: items, controller: controller);
+
+        expect(controller.items, items);
+      },
+    );
+
+    testWidgets(
+      'set initialValue to controller',
+      (tester) async {
+        final value = IncomeCategory.test();
+        final controller = AppSelectorController<IncomeCategory>(
+          displayAsString: (it) => it.name,
+        );
+        await pumpWidget(tester, initialValue: value, controller: controller);
+
+        expect(controller.value, value);
+      },
+    );
+  });
+
+  group('Side Effects', () {
+    testWidgets('set isLoading when status is loading', (tester) async {
+      whenListen(
+        mockIncomeCategoriesBloc,
+        Stream<IncomeCategoriesState>.fromIterable([
+          const IncomeCategoriesState(status: IncomeCategoriesStatus.loading),
+        ]),
+        initialState: const IncomeCategoriesState(),
+      );
+      final controller = AppSelectorController<IncomeCategory>(
+        displayAsString: (it) => it.name,
+      );
+      await pumpWidget(tester, controller: controller);
+      await tester.pumpAndSettle();
+
+      expect(controller.isLoading, isTrue);
+    });
+
+    testWidgets('set items when status is loaded', (tester) async {
+      final items = List<IncomeCategory>.generate(
+        2,
+        (i) => IncomeCategory.test(),
+      );
+      whenListen(
+        mockIncomeCategoriesBloc,
+        Stream<IncomeCategoriesState>.fromIterable([
+          IncomeCategoriesState(
+            categories: items
+                .map((it) => IncomeCategoryWithState(category: it))
+                .toList(),
+            status: IncomeCategoriesStatus.loaded,
+          ),
+        ]),
+        initialState: const IncomeCategoriesState(),
+      );
+      final controller = AppSelectorController<IncomeCategory>(
+        displayAsString: (it) => it.name,
+      );
+      await pumpWidget(tester, controller: controller);
+      await tester.pumpAndSettle();
+
+      expect(controller.items, items);
+      expect(controller.status, SelectorStatus.idle);
+    });
+
+    testWidgets('set lastError when status is failure', (tester) async {
+      whenListen(
+        mockIncomeCategoriesBloc,
+        Stream<IncomeCategoriesState>.fromIterable([
+          IncomeCategoriesState(
+            exception: AppException.test(),
+            status: IncomeCategoriesStatus.failure,
+          ),
+        ]),
+        initialState: const IncomeCategoriesState(),
+      );
+      final controller = AppSelectorController<IncomeCategory>(
+        displayAsString: (it) => it.name,
+      );
+      await pumpWidget(tester, controller: controller);
+      await tester.pumpAndSettle();
+
+      expect(controller.lastError, AppException.test());
+      expect(controller.status, SelectorStatus.error);
+    });
+  });
+
+  group('Interactions', () {
+    testWidgets('adds IncomeCategoriesBloc.search on search', (tester) async {
+      whenListen(
+        mockIncomeCategoriesBloc,
+        const Stream<IncomeCategoriesState>.empty(),
+        initialState: const IncomeCategoriesState(),
+      );
+      await pumpWidget(tester);
+
+      final selectorFinder = find.byType(AppSelector<IncomeCategory>);
+      expect(selectorFinder, findsOneWidget);
+
+      await tester.tap(selectorFinder);
+      await tester.pumpAndSettle();
+
+      final searchFieldFinder = find.descendant(
+        of: find.byType(Column),
+        matching: find.byType(TextFormField),
+      );
+      expect(searchFieldFinder, findsOneWidget);
+
+      await tester.enterText(searchFieldFinder, 'Search');
+      await tester.pumpAndSettle();
+
+      verify(
+        () => mockIncomeCategoriesBloc.add(
+          const IncomeCategoriesEvent.search(query: 'Search'),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('adds IncomeCategoriesBloc.load when pressed', (tester) async {
+      whenListen(
+        mockIncomeCategoriesBloc,
+        const Stream<IncomeCategoriesState>.empty(),
+        initialState: const IncomeCategoriesState(),
+      );
+      await pumpWidget(tester);
+
+      final selectorFinder = find.byType(AppSelector<IncomeCategory>);
+      expect(selectorFinder, findsOneWidget);
+
+      await tester.tap(selectorFinder);
+      await tester.pumpAndSettle();
+
+      verify(
+        () => mockIncomeCategoriesBloc.add(
+          const IncomeCategoriesEvent.load(),
+        ),
+      ).called(1);
+    });
+  });
+}

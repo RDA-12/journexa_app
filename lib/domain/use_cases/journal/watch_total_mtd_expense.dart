c25f257f07daccb4203aa53105c766471c3cbc60
R-12 <ridwandani12@gmail.com>
2026-09-13 23:26:02 +0700

    feat: use case to watch total mtd expense

---
+++ lib/domain/use_cases/journal/watch_total_mtd_expense.dart
@@ -0,0 +1,59 @@
+import 'package:decimal/decimal.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:journexa_app/domain/entities/account.dart';
+import 'package:journexa_app/domain/repositories/i_journal_repository.dart';
+import 'package:journexa_app/domain/use_cases/base_use_case.dart';
+import 'package:journexa_app/shared/app_logger.dart';
+import 'package:journexa_app/shared/app_result.dart';
+
+part 'watch_total_mtd_expense.freezed.dart';
+
+/// Params for [WatchTotalMTDExpenseUseCase]
+@freezed
+sealed class WatchTotalMTDExpenseParams with _$WatchTotalMTDExpenseParams {
+  const factory WatchTotalMTDExpenseParams({
+    required DateTime targetDate,
+  }) = _WatchTotalMTDExpenseParams;
+}
+
+/// Use case to get total month to date (MTD) expense balance
+@lazySingleton
+class WatchTotalMTDExpenseUseCase
+    with Loggable
+    implements StreamBaseUseCase<WatchTotalMTDExpenseParams, Decimal> {
+  /// Creates new [WatchTotalMTDExpenseUseCase]
+  WatchTotalMTDExpenseUseCase({
+    required this._journalRepository,
+  });
+
+  @override
+  String get logTag => 'WatchTotalMTDExpenseUseCase';
+
+  final IJournalRepository _journalRepository;
+
+  @override
+  Stream<AppResult<Decimal>> execute(
+    WatchTotalMTDExpenseParams params, {
+    required String traceId,
+  }) {
+    final targetDate = params.targetDate;
+    final startDate = DateTime(targetDate.year, targetDate.month);
+    final toDate = DateTime(
+      targetDate.year,
+      targetDate.month,
+      targetDate.day,
+      23,
+      59,
+      59,
+      999,
+    );
+    logInfo('Starts watch total expense balance', traceId: traceId);
+    return _journalRepository.watchAccountBalance(
+      account: SystemDefinedAccount.expenseParent,
+      from: startDate,
+      to: toDate,
+      traceId: traceId,
+    );
+  }
+}

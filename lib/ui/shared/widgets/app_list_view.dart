09e0d3ffdf597059223ff603066d1e81aa8aa300
R-12 <ridwandani12@gmail.com>
2026-09-15 20:26:32 +0700

    feat(app list view): allows making list view not scrollable

--- lib/ui/shared/widgets/app_list_view.dart
+++ lib/ui/shared/widgets/app_list_view.dart
@@ -7,6 +7,7 @@ class AppListView<T> extends StatelessWidget {
   const AppListView({
     required this.items,
     required this.itemBuilder,
+    this.isScrollable = true,
     super.key,
   });
 
@@ -16,9 +17,17 @@ class AppListView<T> extends StatelessWidget {
   /// Builder function to create widget from item
   final Widget Function(BuildContext, int) itemBuilder;
 
+  /// Whether let this list view to be scrollable or not.
+  ///
+  /// Its useful when using this as a child of another scrollview. By setting
+  /// this to true, it let its parent to be scrollable
+  final bool isScrollable;
+
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
+      shrinkWrap: !isScrollable,
+      physics: isScrollable ? null : const NeverScrollableScrollPhysics(),
       itemCount: items.length,
       separatorBuilder: (_, _) => const SizedBox(height: 8),
       itemBuilder: itemBuilder,
--- test/ui/shared/widgets/app_list_view_test.dart
+++ test/ui/shared/widgets/app_list_view_test.dart
@@ -7,11 +7,15 @@ import '../../util.dart';
 void main() {
   final items = List.generate(5, (index) => '$index');
 
-  Future<void> pumpWidget(WidgetTester tester) {
+  Future<void> pumpWidget(
+    WidgetTester tester, {
+    bool? isScrollable,
+  }) {
     return pumpForWidgetTest(
       tester,
       locale: const Locale('en'),
       widget: AppListView(
+        isScrollable: isScrollable ?? true,
         items: items,
         itemBuilder: (context, index) {
           final item = items[index];
@@ -32,6 +36,20 @@ void main() {
         for (var i = 0; i < items.length; i++) {
           expect(find.text(items[i]), findsOneWidget);
         }
+      },
+    );
+
+    testWidgets(
+      'set shrinkWrap to true and physics to NeverScrollableScrollPhysics '
+      'when isScrollable is false',
+      (tester) async {
+        await pumpWidget(tester, isScrollable: false);
+
+        final finder = find.byType(ListView);
+        expect(finder, findsOneWidget);
+        final widget = tester.widget<ListView>(finder);
+        expect(widget.shrinkWrap, isTrue);
+        expect(widget.physics, const NeverScrollableScrollPhysics());
       },
     );
   });

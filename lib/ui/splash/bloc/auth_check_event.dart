04c0135599f2e560acba11c8340a5c60e2f56e20
R-12 <ridwandani12@gmail.com>
2026-09-18 00:58:36 +0700

    refactor(auth check bloc): remove unnecessary bloc

--- lib/ui/splash/bloc/auth_check_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'auth_check_bloc.dart';
-
-/// Events for [AuthCheckBloc]
-@freezed
-class AuthCheckEvent with _$AuthCheckEvent {
-  /// Event to start checking auth state
-  const factory started() = _Started;
-}

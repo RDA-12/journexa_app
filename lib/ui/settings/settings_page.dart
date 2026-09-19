b53f61544bb22c279f678ae6d5c66697f5c08546
R-12 <ridwandani12@gmail.com>
2026-09-19 17:51:08 +0700

    chore(settings): padding on page

--- lib/ui/settings/settings_page.dart
+++ lib/ui/settings/settings_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:journexa_app/ui/router/router.dart';
 import 'package:journexa_app/ui/shared/l10n/l10n.dart';
+import 'package:journexa_app/ui/shared/widgets/widgets.dart';
 
 /// Page to shows user' settings
 class SettingsPage extends StatelessWidget {
@@ -15,41 +16,44 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(context.l10n.settingsTitle),
       ),
-      body: Column(
-        spacing: 24,
-        children: [
-          Column(
-            spacing: 8,
-            children: [
-              ListTile(
-                leading: const Icon(Icons.payments_rounded),
-                title: Text(
-                  context.l10n.settingsIncomeCategoriesLabel,
-                  semanticsLabel:
-                      context.l10n.settingsIncomeCategoriesSemantics,
+      body: Padding(
+        padding: context.pagePadding,
+        child: Column(
+          spacing: 24,
+          children: [
+            Column(
+              spacing: 8,
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.payments_rounded),
+                  title: Text(
+                    context.l10n.settingsIncomeCategoriesLabel,
+                    semanticsLabel:
+                        context.l10n.settingsIncomeCategoriesSemantics,
+                  ),
+                  onTap: () {
+                    unawaited(
+                      const IncomeCategoriesListRoute().push<void>(context),
+                    );
+                  },
                 ),
-                onTap: () {
-                  unawaited(
-                    const IncomeCategoriesListRoute().push<void>(context),
-                  );
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.receipt_long_rounded),
-                title: Text(
-                  context.l10n.settingsExpenseCategoriesLabel,
-                  semanticsLabel:
-                      context.l10n.settingsExpenseCategoriesSemantics,
+                ListTile(
+                  leading: const Icon(Icons.receipt_long_rounded),
+                  title: Text(
+                    context.l10n.settingsExpenseCategoriesLabel,
+                    semanticsLabel:
+                        context.l10n.settingsExpenseCategoriesSemantics,
+                  ),
+                  onTap: () async {
+                    unawaited(
+                      const ExpenseCategoriesListRoute().push<void>(context),
+                    );
+                  },
                 ),
-                onTap: () async {
-                  unawaited(
-                    const ExpenseCategoriesListRoute().push<void>(context),
-                  );
-                },
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
